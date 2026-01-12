96c9aa8e19fe2894732122e034a67ec6610c79c8
Sujal Mishra <sujalmishra68@gmail.com>
2026-01-12 22:56:28 +0530

    12jan2026

---
+++ StringFormat.java
@@ -0,0 +1,10 @@
+public class StringFormat{
+      public static void main(String[] args) {
+          String  name="sujal";
+          int marks=45;
+          // first method to declear the string
+        System.out.println("Hello "+name+" your marks is "+marks);
+          // second method to declear 
+        System.out.printf("Hello %s, your makrs is%d" ,name, marks);
+    }
+}
