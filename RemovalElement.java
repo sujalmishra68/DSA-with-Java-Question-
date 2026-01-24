168496720a025862da750d4d5c09992479cd2c49
Sujal Mishra <sujalmishra68@gmail.com>
2026-01-24 23:16:38 +0530

    24jan2026

---
+++ RemovalElement.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class removeElement {
+
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    // 🔥 MAIN FUNCTION (ENTRY POINT)
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // input size
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        // input array
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        // input val
+        int val = sc.nextInt();
+
+        int k = removeElement(nums, val);
+
+        // output
+        System.out.println(k);
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
+
