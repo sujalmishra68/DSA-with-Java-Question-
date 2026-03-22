9c6fe62c39a59801d6722113f068ca356614c9a0
Sujal Mishra <sujalmishra68@gmail.com>
2026-03-23 00:48:33 +0530

    23mar2026

--- sum.java
+++ sum.java
@@ -1,18 +1,25 @@
-// sum of array
+// sum of array using function call
  import java.util.Scanner;
 public class sum {
-public static void main(String[] args) {
-    int n, sum = 0;
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the number of elements in the array:");
-    n = sc.nextInt();
-    int[] arr = new int[n];
-    System.out.println("Enter the elements of the array:");
-    for (int i = 0; i < n; i++) {
-        arr[i] = sc.nextInt();
-        sum += arr[i];
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int result = sumArray(arr);
+        System.out.println("The sum of the array is: " + result);
     }
-    System.out.println("The sum of the array is: " + sum);
+
+    public static int sumArray(int[] array) {
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        return sum;
+    }
 }
-    
-}
+ 
